25d8360abec1036e3b40e252e3762b24e54bf0f6
Marethyu9999 <66386137+Marethyu9999@users.noreply.github.com>
2021-08-25 16:06:33 +0200

    Zylinder Anfang
    Ich habe einen Anfang bei zylinder.py gemacht.

---
+++ zylinder.py
@@ -0,0 +1,3 @@
+def zylinderrechnung():
+    print("Welche Daten hast du?\n[1] Radius\n[2] Höhe\n[3] Durchmesser\n[4] Umfang\n[5] Grundfläche\n[6] Mantelfläche\n[7] Oberfläche\n[8] Volumen\nZahlen von Werten die sie besitzen nehmen(Von links nach rechts).")
+    int(input("Werte Code: "))
