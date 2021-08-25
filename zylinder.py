24f93e7b1ea002f9fd502706436a6baa62178b29
Marethyu9999 <66386137+Marethyu9999@users.noreply.github.com>
2021-08-25 16:11:18 +0200

    Update zylinder.py

--- zylinder.py
+++ zylinder.py
@@ -1,3 +1,3 @@
 def zylinderrechnung():
-    print("Welche Daten hast du?\n[1] Radius\n[2] Höhe\n[3] Durchmesser\n[4] Umfang\n[5] Grundfläche\n[6] Mantelfläche\n[7] Oberfläche\n[8] Volumen\nZahlen von Werten die sie besitzen nehmen(Von links nach rechts).")
+    print("Welche Daten hast du?\n[1] Radius\n[2] Höhe\n[3] Durchmesser\n[4] Umfang\n[5] Grundfläche\n[6] Mantelfläche\n[7] Oberfläche\n[8] Volumen\nZahlen von Werten die sie besitzen nehmen(Von links nach rechts. z.B 12 für Radius + Höhe oder 14 für Radius und Umfang) und sie dann in beschriebener Zahlenform eintragen.")
     int(input("Werte Code: "))
