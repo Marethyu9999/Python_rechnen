9181da2c2df903cd329a288eec6273323abd5fe7
Marethyu9999 <66386137+Marethyu9999@users.noreply.github.com>
2021-08-25 16:28:07 +0200

    zylinder.py v1.1
    Ich habe Radius + Höhe hinzugefügt.

--- zylinder.py
+++ zylinder.py
@@ -1,3 +1,19 @@
 def zylinderrechnung():
+    from math import pi, sqrt
     print("Welche Daten hast du?\n[1] Radius\n[2] Höhe\n[3] Durchmesser\n[4] Umfang\n[5] Grundfläche\n[6] Mantelfläche\n[7] Oberfläche\n[8] Volumen\nZahlen von Werten die sie besitzen nehmen(Von links nach rechts. z.B 12 für Radius + Höhe oder 14 für Radius und Umfang) und sie dann in beschriebener Zahlenform eintragen.")
-    int(input("Werte Code: "))
+    choice = int(input("Werte Code: "))
+
+    if choice == 12:
+        radius = float(input("Radius: "))
+        höhe = float(input("Höhe: "))
+        durchmesser = radius*2
+        umfang =  2*radius*pi
+        grundfläche = radius**2*pi
+        mantelfläche = 2*pi*radius*höhe
+        oberfläche = 2*pi*radius*(radius+höhe)
+        volumen = pi*radius**2*höhe
+
+        print(volumen)
+
+zylinderrechnung()
+input("drücken zum schließen... ")
