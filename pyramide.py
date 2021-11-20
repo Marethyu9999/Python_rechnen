85db6c4a30d2b2540e19542a695ebcab865fc974
Marethyu <matuka.grobecker@gmail.com>
2021-11-20 13:37:11 +0100

    pyramide: ha + G 37

--- pyramide.py
+++ pyramide.py
@@ -189,6 +189,18 @@ def pyramiderechnung(choice, wert1, wert2):
         M = 2*a*ha
         O = (a**2)+2*a*ha
         V = (1/3)*(a**2)*h
+    if choice == 37: # ha + G
+        ha = wert1
+        G = wert2
+        a = sqrt(G)
+        h = sqrt(ha**2-(a**2)/4)
+        s = sqrt((h**2)+(a**2)/2)
+        d = sqrt(a**2+a**2)
+        u = 4*a
+        M = 2*a*ha
+        O = (a**2)+2*a*ha
+        V = (1/3)*(a**2)*h
+    
 
 
     Dictionary = {'a':a, 'h':h, 'ha':ha, 's':s, 'd':d, 'u':u, 'G':G, 'M':M, 'O':O, 'V':V}
