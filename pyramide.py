faf98d4ed7b604ce7db350005cd754ce5ad5a8e4
Marethyu <matuka.grobecker@gmail.com>
2021-11-20 13:39:10 +0100

    pyramide: ha + M 38

--- pyramide.py
+++ pyramide.py
@@ -200,7 +200,17 @@ def pyramiderechnung(choice, wert1, wert2):
         M = 2*a*ha
         O = (a**2)+2*a*ha
         V = (1/3)*(a**2)*h
-    
+    if choice == 38: # ha + M
+        ha = wert1
+        M = wert2
+        a = M/(2*ha)
+        h = sqrt(ha**2-(a**2)/4)
+        s = sqrt((h**2)+(a**2)/2)
+        d = sqrt(a**2+a**2)
+        u = 4*a
+        G = a**2
+        O = (a**2)+2*a*ha
+        V = (1/3)*(a**2)*h
 
 
     Dictionary = {'a':a, 'h':h, 'ha':ha, 's':s, 'd':d, 'u':u, 'G':G, 'M':M, 'O':O, 'V':V}
