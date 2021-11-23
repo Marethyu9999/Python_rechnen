8e506ec3dbe24ed60b1ee3f62c83d70ad38d9f93
Marethyu <matuka.grobecker@gmail.com>
2021-11-23 18:22:01 +0100

    pyramide: 510

--- pyramide.py
+++ pyramide.py
@@ -299,6 +299,17 @@ def pyramiderechnung(choice, wert1, wert2):
         G = a**2
         M = 2*a*ha
         V = (1/3)*(a**2)*h
+    elif choice == 510: # d + V
+        d = wert1
+        V = wert2
+        a = sqrt(d**2/2)
+        h = 3*V/a**2
+        ha = sqrt(h**2+((a/2)**2))
+        s = sqrt((h**2)+(a**2)/2)
+        u = 4*a
+        G = a**2
+        M = 2*a*ha
+        O = (a**2)+2*a*ha
 
 
     Dictionary = {'a':a, 'h':h, 'ha':ha, 's':s, 'd':d, 'u':u, 'G':G, 'M':M, 'O':O, 'V':V}
