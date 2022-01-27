1258f5d3f3f748562370d1d6e5473a48f587d88c
Marethyu <matuka.grobecker@gmail.com>
2022-01-27 23:29:05 +0100

    trying to use it as class
    not really working

--- quadratische-gleichung.py
+++ quadratische-gleichung.py
@@ -1,12 +1,16 @@
-def quadratischegleichung(a, b, c):
-    states = {}
-    if 0<a<1:
-        states["gestaucht"] = True
-    elif a>1:
-        states["gestreckt"] = True
-    if a<0:
-        states["Richtung der Öffnung"] = "unten"
+class quadratischegleichung(a, b, c):
+    def statecheck(a):
+        # checking wich tags to apply to the formular
+        states = {}
+        if 0<a<1:
+            states["gestaucht"] = True
+        elif a>1:
+            states["gestreckt"] = True
+        if a<0:
+            states["Richtung der Öffnung"] = "unten"
+        return(states)
     
-    return(states)
-
-print(quadratischegleichung(-5, 2, 3))
+    def calculation(a,b,c,xstart,xend):
+        for x in range(xstart, xend):
+            a*x**2+b*x+c
+    
