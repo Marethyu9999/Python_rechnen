f43c437f1fbe43fdceece20cf7615337439adf86
Marethyu <matuka.grobecker@gmail.com>
2022-01-29 00:15:38 +0100

    adding a checker which is tagging the formular
    tf am i even doing here

--- quadratische-gleichung.py
+++ quadratische-gleichung.py
@@ -1,9 +1,21 @@
 import asyncio
 
 async def input():
-    # Eingabe der Variablen a, b, c
     # input of variables a,b,c
     a = float(input("a: "))
     b = float(input("b: "))
     c = float(input("c: "))
     return a, b, c
+
+async def tags(a):
+    # adding tags to the variables based on the formulas: a>0, 0<a<1, a<0
+    dir = {}
+    if a > 0:
+        dir["gestreckt"] = True
+    elif a < 0 < 1:
+        dir["gestaucht"] = True
+    if a < 0:
+        dir["negativ"] = True
+    return dir
+
+
