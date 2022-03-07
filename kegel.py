0698c862052d2d562a162439ff1739260840c465
Marethyu <eldingar@disroot.org>
2022-03-07 19:02:49 +0100

    kegel - radius + height

--- kegel.py
+++ kegel.py
@@ -3,5 +3,13 @@ def kegel(code, var1, var2):
     from os import system
     clear = lambda: system('cls')
     clear()
+    result = {}
     
-    
+    if code == 12: # Radius + Höhe
+        result["Radius"] = var1
+        result["Höhe"] = var2
+        result["Seite s"] = sqrt(var1**2 + var2**2)
+        result["Mantelfläche M"] = pi * var1 * result["Seite s"]
+        result["Grundfläche G"] = pi * var1**2
+        result["Volumen V"] = 1/3 * result["Grundfläche G"] * result["Höhe"]
+        result["Oberfläche O"] = result["Mantelfläche M"] + result["Grundfläche G"]
