4b7f1ea0ff5f62b26826c5e375cefb0159cf723c
Marethyu <eldingar@disroot.org>
2022-03-07 19:18:37 +0100

    kegel - radius + volumen

--- kegel.py
+++ kegel.py
@@ -21,3 +21,11 @@ def kegel(code, var1, var2):
         result["Grundfläche G"] = pi * var1**2
         result["Volumen V"] = 1/3 * result["Grundfläche G"] * result["Höhe"]
         result["Oberfläche O"] = result["Mantelfläche M"] + result["Grundfläche G"]
+    if code == 14: # Radius + Volumen
+        result["Radius"] = var1
+        result["Volumen V"] = var2
+        result["Grundfläche G"] = pi * result["Radius"]
+        result["Höhe h"] = result["Volumen V"] / (1/3) / result["Grundfläche G"]
+        result["Seite s"] = sqrt(var1**2 * result["Höhe h"]**2)
+        result["Mantelfläche M"] = pi * var1 * result["Seite s"]
+        result["Oberfläche O"] = result["Mantelfläche M"] + result["Grundfläche G"]
