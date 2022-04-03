b65aa64083a2077c370e9bbf299b3bdf7ab1ed05
Marethyu9999 <matuka.grobecker@gmail.com>
2022-04-03 16:39:17 +0200

    changed if to elif

--- kegel.py
+++ kegel.py
@@ -13,7 +13,7 @@ def kegel(code, var1, var2):
         result["Grundfläche G"] = pi * var1**2
         result["Volumen V"] = 1/3 * result["Grundfläche G"] * result["Höhe"]
         result["Oberfläche O"] = result["Mantelfläche M"] + result["Grundfläche G"]
-    if code == 13: # Radius + Seite s
+    elif code == 13: # Radius + Seite s
         result["Radius"] = var1
         result["Seite s"] = var2
         result["Höhe"] = sqrt(var2**2 - var1**2)
@@ -21,7 +21,7 @@ def kegel(code, var1, var2):
         result["Grundfläche G"] = pi * var1**2
         result["Volumen V"] = 1/3 * result["Grundfläche G"] * result["Höhe"]
         result["Oberfläche O"] = result["Mantelfläche M"] + result["Grundfläche G"]
-    if code == 14: # Radius + Volumen
+    elif code == 14: # Radius + Volumen
         result["Radius"] = var1
         result["Volumen V"] = var2
         result["Grundfläche G"] = pi * result["Radius"]
@@ -29,3 +29,5 @@ def kegel(code, var1, var2):
         result["Seite s"] = sqrt(var1**2 * result["Höhe h"]**2)
         result["Mantelfläche M"] = pi * var1 * result["Seite s"]
         result["Oberfläche O"] = result["Mantelfläche M"] + result["Grundfläche G"]
+
+    return result
