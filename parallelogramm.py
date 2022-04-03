2f670f0504de9be6b0f15ef1255ba37b9c700fea
Marethyu9999 <matuka.grobecker@gmail.com>
2022-04-03 18:02:04 +0200

    parallelogramm.py start, .gitignore update
    added more stuff to gitignore
    started making parralelogramm shit
    https://iron-danthus-f3e.notion.site/Parallelogramm-675af6416b6e420d9ad3b4c51fa06bfd

---
+++ parallelogramm.py
@@ -0,0 +1,8 @@
+def parallelogram():
+    from math import cos,sin
+    inp1 = float(input("Input 1: "))
+    inp2 = float(input("Input 2: "))
+    inp3 = int(input("Method: "))
+
+    if inp3 == 12: # Seite a + Seite b
+        höhe1 = 1
